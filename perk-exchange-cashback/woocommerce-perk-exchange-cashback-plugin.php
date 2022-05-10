521d295a275f199e79fd558dd1263fc1d2491386
Geoff Whittington <geoff.whittington@gmail.com>
2022-05-10 10:13:36 -0400

    formatting changes

--- perk-exchange-cashback/woocommerce-perk-exchange-cashback-plugin.php
+++ perk-exchange-cashback/woocommerce-perk-exchange-cashback-plugin.php
@@ -8,37 +8,37 @@
  * Author URI: https://perk.exchange/
  * Version: 1.0
  */
-if (!class_exists('WC_perk_exchange_cashback_plugin')) :
-  class WC_perk_exchange_cashback_plugin
-  {
-    /**
-     * Construct the plugin.
-     */
-    public function __construct()
+if (!class_exists('WC_Perk_Exchange_Cashback_Plugin')) :
+    class WC_Perk_Exchange_Cashback_Plugin
     {
-      add_action('plugins_loaded', array($this, 'init'));
+        /**
+         * Construct the plugin.
+         */
+        public function __construct()
+        {
+            add_action('plugins_loaded', array($this, 'init'));
+        }
+        /**
+         * Initialize the plugin.
+         */
+        public function init()
+        {
+            // Checks if WooCommerce is installed.
+            if (class_exists('WC_Integration')) {
+                // Include our integration class.
+                include_once 'class-wc-perk-exchange-cashback-integration.php';
+                // Register the integration.
+                add_filter('woocommerce_integrations', array($this, 'add_integration'));
+            }
+        }
+        /**
+         * Add a new integration to WooCommerce.
+         */
+        public function add_integration($integrations)
+        {
+            $integrations[] = 'WC_Perk_Exchange_Cashback_Integration';
+            return $integrations;
+        }
     }
-    /**
-     * Initialize the plugin.
-     */
-    public function init()
-    {
-      // Checks if WooCommerce is installed.
-      if (class_exists('WC_Integration')) {
-        // Include our integration class.
-        include_once 'class-wc-perk-exchange-cashback-integration.php';
-        // Register the integration.
-        add_filter('woocommerce_integrations', array($this, 'add_integration'));
-      }
-    }
-    /**
-     * Add a new integration to WooCommerce.
-     */
-    public function add_integration($integrations)
-    {
-      $integrations[] = 'WC_Perk_Exchange_Cashback_Integration';
-      return $integrations;
-    }
-  }
-  $WC_my_custom_plugin = new WC_perk_exchange_cashback_plugin(__FILE__);
+    $WC_my_custom_plugin = new WC_Perk_Exchange_Cashback_Plugin(__FILE__);
 endif;
