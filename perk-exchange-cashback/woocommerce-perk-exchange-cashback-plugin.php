edbc34e0ad1e889ce8c2c60be5c329f00b94e82e
geoffwhittington <geoffwhittington@users.noreply.github.com>
2022-05-14 14:46:06 -0400

    Update woocommerce-perk-exchange-cashback-plugin.php

--- perk-exchange-cashback/woocommerce-perk-exchange-cashback-plugin.php
+++ perk-exchange-cashback/woocommerce-perk-exchange-cashback-plugin.php
@@ -4,7 +4,7 @@
  * Plugin Name: Perk.Exchange Crypto Cashback
  * Plugin URI: https://perkexchange.gitbook.io/docs/master/cashback/woo-commerce
  * Description: Plugin to give crypto cashbacks for WooCommerce orders.
- * Author:  Perk.Exchange
+ * Author: Perk.Exchange
  * Author URI: https://perk.exchange/
  * Version: 1.0.0
  */
